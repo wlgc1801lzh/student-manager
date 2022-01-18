11cf2595ab6de25c6bff8fd587eeb66cdb21019a
空影星辉 <wlgc1801lzh@163.com>
2022-01-18 18:10:10 +0800

    修改后端项目名

--- student-manager-server/src/main/java/student/StudentManagerApplication.java
+++ student-manager-server/src/main/java/student/StudentManagerApplication.java
@@ -3,10 +3,8 @@ package student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableJpaAuditing
-@EnableRedisHttpSession
 @SpringBootApplication
 public class StudentManagerApplication {
     public static void main(String[] args) {
